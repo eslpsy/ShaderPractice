3c29fbdd823009632c245e03e7525392cf1b0506
eslpsy <zhangjianfeng415@gmail.com>
2017-10-26 19:20:57 +0800

    cubemap

---
+++ shaderPractice/Assets/Editor/GenerateStaticCubemap.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GenerateStaticCubemap : ScriptableWizard {
+
+    public Transform renderPosition;
+    public Cubemap cubemap;
+
+    private void OnWizardUpdate()
+    {
+        helpString = "select transform to render" + "from and cubemap to render into";
+
+        if (null != renderPosition && null != cubemap)
+        {
+            isValid = true; // 如果isValid为true，可以执行OnWizardCreate
+        }
+        else
+        {
+            isValid = false;
+        }
+    }
+
+    private void OnWizardCreate()
+    {
+        GameObject go = new GameObject("CubeCam", typeof(Camera));
+
+        go.transform.position = renderPosition.position;
+        go.transform.rotation = Quaternion.identity;
+
+        go.GetComponent<Camera>().RenderToCubemap(cubemap);
+
+        DestroyImmediate(go);
+    }
+
+    [MenuItem("ShaderPractice/Render Cubemap")]
+    static void RenderCubemap()
+    {
+        ScriptableWizard.DisplayWizard("Render CubeMap", typeof(GenerateStaticCubemap), "Render!");
+    }
+}
---
+++ shaderPractice/Assets/shader/baseShader/cubemapShader.shader
@@ -0,0 +1,36 @@
+﻿Shader "Custom/cubemapShader" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_Cubemap("Cube", CUBE) = "" {}
+		_ReflAmount("Reflection Amount", Range(0.01, 1)) = 0.5
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		CGPROGRAM
+		
+		#pragma surface surf Lambert
+
+		sampler2D _MainTex;
+		samplerCUBE _Cubemap;
+
+		struct Input {
+			float2 uv_MainTex;
+			float3 worldRefl;
+		};
+
+		fixed4 _Color;
+		float _ReflAmount;
+
+		void surf (Input IN, inout SurfaceOutput o) {
+			fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+			o.Emission = texCUBE(_Cubemap, IN.worldRefl).rgb * _ReflAmount;
+			o.Albedo = c.rgb;
+			o.Alpha = c.a;
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
